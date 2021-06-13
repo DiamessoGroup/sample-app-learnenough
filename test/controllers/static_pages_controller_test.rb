1ce6fbe4dbe61e533a25c2f2bc18962e235745b8
Krishna Diamesso <krishnadiamesso@gmail.com>
2021-06-12 23:58:56 -0500

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,6 +11,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
+    end
+
   end
 
   test 'should get home' do
